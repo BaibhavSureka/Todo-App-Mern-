7071f4480f6932853ead1f871e3bfc2b5df76761
Baibhav Sureka <83425243+BaibhavSureka@users.noreply.github.com>
2024-06-19 22:57:49 +0530

    Remove existing code

--- Backend/config.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const MONGO_URI = process.env.MONGO_URI;
-module.exports = {
-    JWT_SECRET : "Complicated Key",
-    MONGO_URI : MONGO_URI,
-}
--- Frontend/config.js
+++
@@ -1,3 +0,0 @@
-export const config = {
-    apiUrl: "https://to-do-app-topaz-sigma.vercel.app"
-}
