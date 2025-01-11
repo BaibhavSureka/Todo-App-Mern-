b16c3a5da17b89eca6d870fd2253762b5c5950aa
Baibhav Sureka <83425243+BaibhavSureka@users.noreply.github.com>
2025-01-11 09:53:53 -0800

    Update axios.jsx

--- frontend/src/Axios/axios.jsx
+++ frontend/src/Axios/axios.jsx
@@ -1,5 +1,8 @@
-import axios from "axios"
+import axios from "axios";
+
+// Use an environment variable for the base URL
 const instance = axios.create({
-    baseURL:"http://localhost:8000/api"
-})
-export default instance
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000/api", // Default to localhost if the environment variable is not set
+});
+
+export default instance;
